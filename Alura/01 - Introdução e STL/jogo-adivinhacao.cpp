b73043348badd592054a264bcfa0c14d42174acd
r0b14-ubuntu <56169778+r0b14@users.noreply.github.com>
2025-02-01 01:55:42 -0300

    add functions news
    adicionada uma função que faz a contagem dos pontos.

--- Alura/01 - Introdução e STL/jogo-adivinhacao.cpp
+++ Alura/01 - Introdução e STL/jogo-adivinhacao.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -10,27 +11,43 @@ int main() {
   cout << "*Bem-vindos ao jogo da adivinhação*" << endl;
   cout << "***********************************" << endl;
 
-  int number_secret = 55, number_user = 0;
-  int counter = 1;
+  int number_secret = 55, number_user = 0, counter = 1;
+  int point = 1000;
+  bool acertou = false;
+
+  // Coletando os dados - primeira rodada
   cout << "Digite o número secreto: ";
   cin >> number_user;
+  
+  if (number_user < 1000) {
+    while (!acertou) {
+      if (number_user > number_secret) {
+        cout << "O número digitado é maior que o número secreto.\n";
+        cout << "Tentativa " << counter << endl;
+        cout << "Digite um número menor: ";
+        cin >> number_user;
 
-  while (number_user != number_secret) {
-    counter++;
-    if (number_user > number_secret) {
-      cout << "O número digitado é maior que o número secreto.\n";
-      cout << "Tentativa " << counter << endl;
-      cout << "Digite um número menor: ";
-      cin >> number_user;
-    } else {
-      cout << "O número digitado é menor que o número secreto.\n";
-      cout << "Tentativa " << counter << endl;
-      cout << "Digite um número maior: ";
-      cin >> number_user;
+        point -= fabs((number_user - number_secret) / 2);
+        counter++;
+      } else if (number_user < number_secret) {
+        cout << "O número digitado é menor que o número secreto.\n";
+        cout << "Tentativa " << counter << endl;
+        cout << "Digite um número maior: ";
+        cin >> number_user;
+
+        point -= fabs((number_user - number_secret) / 2);
+        counter++;
+      }
+      if (number_user == number_secret) {
+        cout << "Parabéns! Você acertou o número em " << counter << " tentativa.\n"
+            << "A sua pontuação: " << point;
+        acertou = true;
+      }
     }
-  } 
-  cout << "Parabéns! Você acertou o número em \n"
-  << counter << " tentativa.";
-
+  } else {
+      cout << "Digite um número menor que 1000.\n";
+    }
+    //cout << "Parabéns! Você acertou o número em " << counter << " tentativa.\n"
+    //     << "A sua pontuação: " << point;
   return 0;
 }
